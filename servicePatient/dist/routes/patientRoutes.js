6c4f70cb4ccfcd4c197913232a819a7f8021a4ea
nourhen <nourhenjlassi79@gmail.com>
2024-12-09 15:16:30 +0100

    front

--- servicePatient/dist/routes/patientRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const patientController_1 = require("../controllers/patientController");
-const router = (0, express_1.Router)();
-// Définir les routes
-router.get('/', patientController_1.getPatients); // Obtenir tous les patients
-exports.default = router;
--- servicePatient/src/config/database.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('projetclinique', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-export default sequelize;
